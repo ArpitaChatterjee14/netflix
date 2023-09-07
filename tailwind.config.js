30813bf12a7d5d7b50d3de08b85e4f6b07b2fb82
NEWUSER <arpitachatterjee47@gmail.com>
2023-09-07 19:34:34 +0530

    committing changes

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], //we are adding this to tell tailwind just to update these extensions like under folder src and for any further folders to search for js and html files to apply tailwind
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
